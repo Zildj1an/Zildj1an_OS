99787767610705260420219de2074357eaa0f1be
Carlos Bilbao <Zildj1an@users.noreply.github.com>
2019-06-17 13:31:29 +0200

    Hello World

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -10,13 +10,13 @@ int main_k(void){
 	 SCREEN_BUFFER = (UINT16*) VGA_ADDRESS;
 
 	 SCREEN_BUFFER[0] = VGA_entry('W');
- 	 SCREEN_BUFFER[0] = VGA_entry('e');
-	 SCREEN_BUFFER[0] = VGA_entry('l');
-	 SCREEN_BUFFER[0] = VGA_entry('c');
-	 SCREEN_BUFFER[0] = VGA_entry('o');
-	 SCREEN_BUFFER[0] = VGA_entry('m');
-	 SCREEN_BUFFER[0] = VGA_entry('e');
-	 SCREEN_BUFFER[0] = VGA_entry('!');
+ 	 SCREEN_BUFFER[1] = VGA_entry('e');
+	 SCREEN_BUFFER[2] = VGA_entry('l');
+	 SCREEN_BUFFER[3] = VGA_entry('c');
+	 SCREEN_BUFFER[4] = VGA_entry('o');
+	 SCREEN_BUFFER[5] = VGA_entry('m');
+	 SCREEN_BUFFER[6] = VGA_entry('e');
+	 SCREEN_BUFFER[7] = VGA_entry('!');
 
 	while(1){}
 
--- kernel/text.h
+++ kernel/text.h
@@ -1,7 +1,13 @@
 #ifndef _TEXT_H_
 #define _TEXT_H_
 
-#define VGA_ADDRESS 0xB800
+/* VGA Colors */
+#define VGA_ADDRESS 0xB8000
+#define BLACK 0
+#define BLUE  1
+#define GREEN 2
+#define RED   4
+#define BROWN 6
 #define WHITE 15
 
 typedef unsigned short UINT16;
@@ -9,7 +15,8 @@ typedef unsigned short UINT16;
 UINT16* SCREEN_BUFFER;
 
 static UINT16 VGA_entry(unsigned char msg){
-	return (UINT16) msg | (UINT16)WHITE << 8;
+
+	return (UINT16) msg | (UINT16) WHITE << 8;
 }
 
 
